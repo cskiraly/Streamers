2abd0645e65bc6394fdec4c1fa19d6e99b0998a8
Luca Abeni <luca.abeni@unitn.it>
2010-01-27 23:12:39 +0100

    Add include guards

--- loop.h
+++ loop.h
@@ -1,3 +1,7 @@
+#ifndef LOOP_H
+#define LOOP_H
+
 void loop(struct nodeID *s, int period, int buff_size);
 void source_loop(const char *fname, struct nodeID *s, int csize, int chunks);
 
+#endif	/* LOOP_H */
--- streaming.h
+++ streaming.h
@@ -1,5 +1,10 @@
+#ifndef STREAMING_H
+#define STREAMING_H
+
 void stream_init(int size, struct nodeID *myID);
 int source_init(const char *fname, struct nodeID *myID);
 void received_chunk(const uint8_t *buff, int len);
 void send_chunk(const struct nodeID **neighbours, int n);
 void generated_chunk(void);
+
+#endif	/* STREAMING_H */
