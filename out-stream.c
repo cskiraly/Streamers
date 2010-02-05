928dbfc6359f426b126f635502b4233eb9b0583e
Csaba Kiraly <kiraly@disi.unitn.it>
2010-02-05 09:18:25 +0100

    debug output instead of real data only if DEBUGOUT is specified

--- out-stream.c
+++ out-stream.c
@@ -9,7 +9,7 @@ static int outfd = 1;
 
 void chunk_write(int id, const uint8_t *data, int size)
 {
-#ifdef DEBUG
+#ifdef DEBUGOUT
 #define buff_size 8 // HACK!
     fprintf(stderr, "\tOut Chunk[%d] - %d: %s\n", id, id % buff_size, data);
 #else
