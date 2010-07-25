028a5232e56f244f45ebc24f181574aa36fc740a
CsabaKiraly <CsabaKiraly@c54fac2e-1815-407e-9b96-088d06743913>
2010-07-25 20:27:06 +0000

    some comments in input.h
    git-svn-id: https://repository.napa-wine.eu/svn/napawine-software/trunk/Applications/OfferStreamer@1727 c54fac2e-1815-407e-9b96-088d06743913

--- input.h
+++ input.h
@@ -13,6 +13,11 @@ struct input_desc;
 
 struct input_desc *input_open(const char *fname, uint16_t flags, int *fds, int fds_size);
 void input_close(struct input_desc *s);
+
+/*
+ * c: chunk structure to be filled. If c->data = NULL after call, there is no new chunk
+ * Returns: timeout requested till next call to the function, <0 in case of input error
+ */
 int input_get(struct input_desc *s, struct chunk *c);
 
 #endif	/* INPUT_H */
