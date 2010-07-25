7f5321cbaa304e69be159129fa15b652eb44f4e7
CsabaKiraly <CsabaKiraly@c54fac2e-1815-407e-9b96-088d06743913>
2010-07-25 20:26:52 +0000

    upgrading chunk_signaling to new GRAPES and removing useless functions
    git-svn-id: https://repository.napa-wine.eu/svn/napawine-software/trunk/Applications/OfferStreamer@1721 c54fac2e-1815-407e-9b96-088d06743913

--- chunk_signaling.c
+++ chunk_signaling.c
@@ -24,91 +24,11 @@
 #include "chunk_signaling.h"
 #include "msg_types.h"
 #include "net_helper.h"
+#include <trade_sig_ha.h>
 
 #include "streaming.h"
 #include "topology.h"
 #include "dbg.h"
-
-static struct nodeID *localID;
-/*
-
-int sendSignalling(int type, const struct nodeID *to_id, const struct nodeID *owner_id, struct chunkID_set *cset, int max_deliver, int cb_size, int trans_id)
-{
-    int buff_len, meta_len, msg_len, ret;
-    uint8_t *buff;
-    struct sig_nal *sigmex;
-    uint8_t *meta;
-
-    meta = malloc(1024);
-
-    sigmex = (struct sig_nal*) meta;
-    sigmex->type = type;
-    sigmex->max_deliver = max_deliver;
-    sigmex->cb_size = cb_size;
-    sigmex->trans_id = trans_id;
-    meta_len = sizeof(*sigmex)-1;
-      sigmex->third_peer = 0;
-    if (owner_id) {
-      meta_len += nodeid_dump(&sigmex->third_peer, owner_id);
-    }
-
-    buff_len = 1 + chunkID_set_size(cset) * 4 + 16 + meta_len; // this should be enough
-    buff = malloc(buff_len);
-    if (!buff) {
-      fprintf(stderr, "Error allocating buffer\n");
-      return -1;
-    }
-
-    buff[0] = MSG_TYPE_SIGNALLING;
-    msg_len = 1 + encodeChunkSignaling(cset, meta, meta_len, buff+1, buff_len-1);
-    free(meta);
-    if (msg_len <= 0) {
-      fprintf(stderr, "Error in encoding chunk set for sending a buffermap\n");
-      ret = -1;
-    } else {
-      send_to_peer(localID, to_id, buff, msg_len);
-    }
-    ret = 1;
-    free(buff);
-    return ret;
-}*/
-
-/**
- * Send a BufferMap to a Peer.
- *
- * Send (our own or some other peer's) BufferMap to a third Peer.
- *
- * @param[in] to PeerID.
- * @param[in] owner Owner of the BufferMap to send.
- * @param[in] bmap the BufferMap to send.
- * @param[in] trans_id transaction number associated with this send
- * @return 0 on success, <0 on error
- */
-/*
-int sendBufferMap(const struct nodeID *to_id, const struct nodeID *owner_id, struct chunkID_set *bmap, int cb_size, int trans_id) {
-  return sendSignalling(MSG_SIG_BMOFF, to_id, owner_id, bmap, 0, cb_size, trans_id);
-}
-
-int sendMyBufferMap(const struct nodeID *to_id, struct chunkID_set *bmap, int cb_size, int trans_id)
-{
-  return sendBufferMap(to_id, localID, bmap, cb_size, trans_id);
-}
-*/
-
-/*
-int offerChunks(const struct nodeID *to_id, struct chunkID_set *cset, int max_deliver, int trans_id) {
-  return sendSignalling(MSG_SIG_OFF, to_id, NULL, cset, max_deliver, -1, trans_id);
-}
-*/
-/*
-int acceptChunks(const struct nodeID *to_id, struct chunkID_set *cset, int max_deliver, int trans_id) {
-  return sendSignalling(MSG_SIG_ACC, to_id, NULL, cset, max_deliver, -1, trans_id);
-}
-*/
-
-/// ==================== ///
-///        RECEIVE       ///
-/// ==================== ///
 
 void bmap_received(const struct nodeID *fromid, const struct nodeID *ownerid, struct chunkID_set *c_set, int cb_size, int trans_id) {
   struct peer *owner;
@@ -171,60 +91,35 @@ void accept_received(const struct nodeID *fromid, struct chunkID_set *cset, int 
  *
  * @param[in] buff buffer which contains the signaling message
  * @param[in] buff_len length of the buffer
- * @param[in] msgtype type of message in the buffer
- * @param[in] max_deliver deliver at most this number of Chunks
- * @param[in] arg parameters associated to the signaling message
  * @return 0 on success, <0 on error
  */
 
 int sigParseData(const struct nodeID *fromid, uint8_t *buff, int buff_len) {
     struct chunkID_set *c_set;
-    void *meta;
-    int meta_len;
-    struct sig_nal *signal;
-    int sig;
+    struct nodeID *ownerid;
+    enum signaling_type sig_type;
+    int max_deliver = 0, trans_id = 0;
     int ret = 1;
     dprintf("Decoding signaling message...");
-    c_set = decodeChunkSignaling(&meta, &meta_len, buff+1, buff_len-1);
-    dprintf(" SIG_HEADER: len: %d, of which meta: %d\n", buff_len, meta_len);
-    if (!c_set) {
-      fprintf(stdout, "ERROR decoding signaling message\n");
+
+    ret = parseSignaling(buff + 1, buff_len-1, &ownerid, &c_set, &max_deliver, &trans_id, &sig_type);
+    if (ret < 0) {
+      fprintf(stdout, "ERROR parsing signaling message\n");
       return -1;
     }
-    signal = (struct sig_nal *) meta;
-    sig = (int) (signal->type);
-    dprintf("\tSignaling Type %d\n", sig);
-    //MaxDelivery  and Trans_Id to be defined
-    switch (sig) {
-        case MSG_SIG_BMOFF:
-        {
-          int dummy;
-          struct nodeID *ownerid = nodeid_undump(&(signal->third_peer),&dummy);
-          bmap_received(fromid, ownerid, c_set, signal->cb_size, signal->trans_id);
-          nodeid_free(ownerid);
+    switch (sig_type) {
+        case sig_send_buffermap:
+          bmap_received(fromid, ownerid, c_set, trans_id); //FIXME: cb_size has gone from signaling
           break;
-        }
-        case MSG_SIG_OFF:
-          offer_received(fromid, c_set, signal->max_deliver, signal->trans_id);
+        case sig_offer:
+          offer_received(fromid, c_set, max_deliver, trans_id);
           break;
-        case MSG_SIG_ACC:
-          accept_received(fromid, c_set, signal->max_deliver, signal->trans_id);
+        case sig_accept:
+          accept_received(fromid, c_set, chunkID_set_size(c_set), trans_id);
           break;
         default:
           ret = -1;
     }
-    
     chunkID_set_free(c_set);
-    free(meta);
     return ret;
-}
-
-/// ==================== ///
-///          INIT        ///
-/// ==================== ///
-
-int sigInit(struct nodeID *myID)
-{
-  localID = myID;
-  return 1;
 }
--- chunk_signaling.h
+++ chunk_signaling.h
@@ -7,49 +7,6 @@
 #ifndef CHUNK_SIGNALING_H
 #define CHUNK_SIGNALING_H
 
-#include "chunkidset.h"
-
-//Type of signaling message
-//Request a ChunkIDSet
-#define MSG_SIG_REQ 0
-//Diliver a ChunkIDSet (reply to a request)
-#define MSG_SIG_DEL 1
-//Offer a ChunkIDSet
-#define MSG_SIG_OFF 2
-//Accept a ChunkIDSet (reply to an offer)
-#define MSG_SIG_ACC 3
-//Receive the BufferMap
-#define MSG_SIG_BMOFF 4
-//Request the BufferMap
-#define MSG_SIG_BMREQ 5
-
-
-struct sig_nal {
-    uint8_t type;//type of signal.
-    uint8_t max_deliver;//Max number of chunks to deliver.
-    uint16_t cb_size;
-    uint16_t trans_id;//future use...
-    uint8_t third_peer;//for buffer map exchange from other peers, just the first byte!
-} __attribute__((packed));
-
-
 int sigParseData(const struct nodeID *from_id, uint8_t *buff, int buff_len);
-
-//int sendBufferMap(const struct nodeID *to_id, const struct nodeID *owner_id, ChunkIDSet *bmap, int cb_size, int trans_id);
-
-//int sendMyBufferMap(const struct nodeID *to_id, ChunkIDSet *bmap, int cb_size, int trans_id);
-
-//int offerChunks(const struct nodeID *to_id, struct chunkID_set *cset, int max_deliver, int trans_id);
-
-//int acceptChunks(const struct nodeID *to_id, struct chunkID_set *cset, int max_deliver, int trans_id);
-
-/**
-  * Init the chunk signaling stuff...
-  *
-  *
-  * @param myID address of this peer
-  * @return >= 0 on success, <0 on error
-  */
-int sigInit(struct nodeID *myID);
 
 #endif
