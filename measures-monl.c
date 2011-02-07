e07f702ed1f1b525887c1eb96ca0d7c64efb8e22
Csaba Kiraly <kiraly@disi.unitn.it>
2011-02-07 01:55:41 +0100

    turn on capacity measurement

--- measures-monl.c
+++ measures-monl.c
@@ -226,12 +226,12 @@ void add_measures(struct nodeID *id)
        add_measure(&id->mhs[j++], RX_PACKET, DATA | IN_BAND, P2P_PUBLISH_INTERVAL, "RxChunks", stwinavgrate, sizeof(stwinavgrate)/sizeof(enum stat_types), id->addr, MSG_TYPE_CHUNK);	//RxChunks_sum [chunks] RxChunks_rate [chunks/sec]
        add_measure(&id->mhs[j++], TX_PACKET, DATA | IN_BAND, P2P_PUBLISH_INTERVAL, "TxChunks", stwinavgrate, sizeof(stwinavgrate)/sizeof(enum stat_types), id->addr, MSG_TYPE_CHUNK);	//TxChunks_sum [chunks] TxChunks_rate [chunks/sec]
 //	// Capacity
-//	add_measure(&id->mhs[j++], CLOCKDRIFT, PACKET | IN_BAND, 0, NULL, NULL, 0, id->addr, MSG_TYPE_CHUNK);
-//	monSetParameter (id->mhs[j-1], P_CLOCKDRIFT_ALGORITHM, 1);
-//	add_measure(&id->mhs[j++], CORRECTED_DELAY, PACKET | IN_BAND, 0, NULL, NULL, 0, id->addr, MSG_TYPE_CHUNK);
-//	add_measure(&id->mhs[j++], CAPACITY_CAPPROBE, PACKET | IN_BAND, P2P_PUBLISH_INTERVAL, "Capacity", stwinavg, sizeof(stwinavg)/sizeof(enum stat_types), id->addr, MSG_TYPE_CHUNK);	//[bytes/s]
-//	monSetParameter (id->mhs[j-1], P_CAPPROBE_DELAY_TH, -1);
-//	monSetParameter (id->mhs[j-1], P_DEBUG_FILE, 3);
+	add_measure(&id->mhs[j++], CLOCKDRIFT, PACKET | IN_BAND, 0, NULL, NULL, 0, id->addr, MSG_TYPE_CHUNK);
+	monSetParameter (id->mhs[j-1], P_CLOCKDRIFT_ALGORITHM, 1);
+	add_measure(&id->mhs[j++], CORRECTED_DELAY, PACKET | IN_BAND, 0, NULL, NULL, 0, id->addr, MSG_TYPE_CHUNK);
+	add_measure(&id->mhs[j++], CAPACITY_CAPPROBE, PACKET | IN_BAND, P2P_PUBLISH_INTERVAL, "Capacity", stwinavg, sizeof(stwinavg)/sizeof(enum stat_types), id->addr, MSG_TYPE_CHUNK);	//[bytes/s]
+	monSetParameter (id->mhs[j-1], P_CAPPROBE_DELAY_TH, -1); //disable: seems to have  implementation problems
+//	monSetParameter (id->mhs[j-1], P_CAPPROBE_PKT_TH, 5);
 //	monSetParameter (mh, P_CAPPROBE_PKT_TH, 100);
 //	monSetParameter (mh, P_CAPPROBE_IPD_TH, 60);
 //	monPublishStatisticalType(mh, NULL, st , sizeof(st)/sizeof(enum stat_types), repoclient);
